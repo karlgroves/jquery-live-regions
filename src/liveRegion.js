aaaf8caf88e4cdc83ee94ec86d438e46f75163b4
Asa Baylus <asa@baylus.com>
2014-12-26 21:14:34 -0500

    Use CommonJS, AMD or browser globals to create a jQuery plugin

--- src/liveRegion.js
+++ src/liveRegion.js
@@ -1,4 +1,15 @@
-;(function ($, window, undefined) {
+;(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS
+        factory(require('jquery'));
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
     'use strict';
     $.fn.extend({
         /**
@@ -164,4 +175,4 @@
             return this;
         }
     });
-})(jQuery, window);
+}));
